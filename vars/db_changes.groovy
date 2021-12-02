d951178abcefea74e37992f9a4c9daba98557648
BipinChoudhary <74283725+BipinChoudhary@users.noreply.github.com>
2021-12-02 09:03:11 +0530

    Update and rename db_changes.groovy.txt to db_changes.groovy

---
+++ vars/db_changes.groovy
@@ -0,0 +1,13 @@
+node {
+    properties([parameters([choice(choices: ['dev', 'uat', 'prod'], description: 'env', name: 'Env'), string(defaultValue: 'Please enter your db', name: 'database'), string(defaultValue: 'Please enter your script file name', name: 'SQL Scripts File')])])
+    stage('Preparation') { // for display purposes
+        
+    }
+    stage('Build') {
+        echo "new upodate"
+        
+    }
+    stage('Results') {
+       
+    }
+}
