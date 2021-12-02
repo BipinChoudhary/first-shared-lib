f652ff496c6183e71ffe6c7c4a329baac5c4b213
BipinChoudhary <74283725+BipinChoudhary@users.noreply.github.com>
2021-12-02 09:24:40 +0530

    Update db_changes.groovy

--- vars/db_changes.groovy
+++ vars/db_changes.groovy
@@ -1,13 +1,11 @@
-node {
-    properties([parameters([choice(choices: ['dev', 'uat', 'prod'], description: 'env', name: 'Env'), string(defaultValue: 'Please enter your db', name: 'database'), string(defaultValue: 'Please enter your script file name', name: 'SQL Scripts File')])])
-    stage('Preparation') { // for display purposes
-        
-    }
-    stage('Build') {
-        echo "new upodate"
-        
-    }
-    stage('Results') {
-       
+pipeline {
+    agent any
+    stages {
+        stage ('Example') {
+            steps {
+                // log.info 'Starting' 
+                echo "test"
+            }
+        }
     }
 }
